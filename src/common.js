474dd890ab28c76db175e45750e4c579feafe2dc
Gabriel Estevam Maciel <gabrielestevammaciel1@gmail.com>
2022-10-13 22:09:01 -0300

    ajuste mensagem de erro

--- src/common.js
+++ src/common.js
@@ -3,7 +3,12 @@ import { Alert, Platform } from 'react-native'
 const server = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://192.168.15.15:3000'
 
 function showError(error) {
-    Alert.alert('ops! Ocorreu um Problema!', `Mensagem: ${error}`)
+    if(error.response && error.response.data) {
+        Alert.alert('ops! Ocorreu um Problema!', `Mensagem: ${error.response.data}`)
+    }
+    else {
+        Alert.alert('ops! Ocorreu um Problema!', `Mensagem: ${error}`)
+    }
 }
 
 function showSucces(msg) {
